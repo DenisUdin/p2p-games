a8a80ca9d13b0a9d935f03cd3cd342611172acb0
Денис <Denis@Mac.lan>
2025-08-21 21:59:00 +0400

    fix

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755798136|60164789928';
+const CACHE_VERSION = '1755799118|61146767580';
 /** @type {string} */
 const CACHE_PREFIX = 'p2p-games-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
