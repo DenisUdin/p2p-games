fb161b019ffb1cf481243d94f0878cd388e7aa69
Денис <Denis@Mac.lan>
2025-09-12 02:00:19 +0400

    shadows

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1757531774|63161200795';
+const CACHE_VERSION = '1757627998|7761404294';
 /** @type {string} */
 const CACHE_PREFIX = 'p2p-games-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
